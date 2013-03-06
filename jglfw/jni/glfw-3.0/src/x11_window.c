6414d8d4d893851e3754ca71c7a0c6b644fe1b50
NathanSweet <nathan.sweet@gmail.com>
2013-03-06 14:35:29 +0100

    Fixed Linux undecorated. Surprise!

--- jglfw/jni/glfw-3.0/src/x11_window.c
+++ jglfw/jni/glfw-3.0/src/x11_window.c
@@ -46,6 +46,16 @@
 #define Button6            6
 #define Button7            7
 
+typedef struct
+{
+	unsigned long flags;
+	unsigned long functions;
+	unsigned long decorations;
+	long input_mode;
+	unsigned long status;
+} MotifWmHints;
+
+#define MWM_HINTS_DECORATIONS (1L << 1)
 
 // Translates an X Window key to internal coding
 //
@@ -122,10 +132,10 @@ static GLboolean createWindow(_GLFWwindow* window,
                                            &wa);
 
         if (wndconfig->undecorated) {
-            Atom motif_hints_atom = XInternAtom(disp, "_MOTIF_WM_HINTS", False);
+            Atom motif_hints_atom = XInternAtom(_glfw.x11.display, "_MOTIF_WM_HINTS", False);
             MotifWmHints motif_hints;
             motif_hints.flags = MWM_HINTS_DECORATIONS;
-            motif_hints.decorations = dec;
+            motif_hints.decorations = 0;
             XChangeProperty(_glfw.x11.display, window->x11.handle,
                 motif_hints_atom, motif_hints_atom, 32,
                 PropModeReplace,
